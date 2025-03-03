5006c6ab5445785593ab79f1faba1ef8a118066c
AlanoodYassin <65427287+AlanoodYassin@users.noreply.github.com>
2025-03-04 01:37:00 +0200

    setup

---
+++ script.js
@@ -0,0 +1,9 @@
+const myLibrary = [
+    { author: "J.K. Rowling", title: "Harry Potter and the Sorcerer's Stone", numberOfPages: 309, isRead: true, image: "images/jk.jpeg" },
+    { author: "George Orwell", title: "1984", numberOfPages: 328, isRead: false , image: "images/1984.jpg"},
+    { author: "Harper Lee", title: "To Kill a Mockingbird", numberOfPages: 281, isRead: true , image: "images/mocking.jpg" },
+    { author: "F. Scott Fitzgerald", title: "The Great Gatsby", numberOfPages: 180, isRead: false, image: "images/gatsby.jpg" },
+    { author: "J.R.R. Tolkien", title: "The Hobbit", numberOfPages: 310, isRead: true , image: "images/hobbit.jpeg"},
+    { author: "Jane Austen", title: "Pride and Prejudice", numberOfPages: 432, isRead: false, image: "images/Prideandprejudice.png"}
+  ];
+
